867754060b91089c1dac20d665abb2380e7eea9a
nguyenducde <ducde678@gmail.com>
2023-09-19 17:40:52 +0700

    first commit

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,2 @@
-export const API_URL = process.env.API_URL || "http://localhost:5000";
-export const UPLOAD_URL = process.env.UPLOAD_URL || "http://localhost:5000/api";
+export const API_URL = process.env.API_URL || "https://ecomerce-api-vrfd.onrender.com";
+export const UPLOAD_URL = process.env.UPLOAD_URL || "https://ecomerce-api-vrfd.onrender.com/api";
